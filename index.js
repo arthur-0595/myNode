0f16bc02652560875e3398a007693b41e861da9f
Yinyan <751410306@qq.com>
2019-01-24 18:12:23 +0800

    1

--- index.js
+++ index.js
@@ -1,71 +1,28 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-const url = require('url')
-const handlebars = require('handlebars')
-const zlib = require('zlib')
+/*
+ * black 2019/1/24
+ * connect组件
+ * */
+const connect = require('connect');
+let router = require('./router/router').router;
 
-const mime = require('./states/mime.json')
-
-const public_folder = path.join(__dirname, 'node_modules')
-
-let server = http.createServer((req, res) => {
-
-  let pathName = url.parse(req.url).pathname;
-  let realPath = path.join(public_folder, pathName);
-
-  fs.stat(realPath, (err, stats) => {
-    // 如果文件不存在，则返回404
-    if (err) {
-      res.writeHead(404, {
-        'Content-Type': 'text/html'
-      })
-      let templateStr = fs.readFileSync('./template/404.html')
-      let template = handlebars.compile(templateStr.toString())
-      let data = {
-        path: url.parse(req.url).pathname
-      }
-      res.end(template(data))
-    } else {
-      if (stats.isDirectory()) {
-        let templateStr = fs.readFileSync('./template/dirList.html')
-        let template = handlebars.compile(templateStr.toString())
-        let data = {
-          path: path.join(pathName, '/'),
-          title: url.parse(req.url).pathname,
-          files: fs.readdirSync(realPath)
-        }
-        res.writeHead(200, {
-          'Content-Type': 'text/html'
-        })
-        res.end(template(data))
-      } else {
-        let extension = path.extname(pathName);
-        let fileType = mime[extension] || 'text/plain'
-
-        let acceptEncoding = req.headers['accept-encoding'] || '';
-        let compressable = extension.match(/css|js|html|json|xml|md|text/ig)
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', fileType)
-
-        if (compressable && acceptEncoding.match(/\bgzip\b/)) {
-          res.setHeader('Content-Encoding', 'gzip')
-          fs.createReadStream(realPath)
-            .pipe(zlib.createGzip())
-            .pipe(res)
-        } else if (compressable && acceptEncoding.match(/\bdeflate\b/)) {
-          res.setHeader('Content-Encoding', 'deflate')
-          fs.createReadStream(realPath)
-            .pipe(zlib.createDeflate())
-            .pipe(res)
-        } else {
-          fs.createReadStream(realPath)
-            .pipe(res)
-        }
-      }
-
+let routes = {
+  GET: {
+    '/users': (req, res) => {
+      res.end('tobi,loki,ferret');
+    },
+    '/users/:id': (req, res, id) => {
+      res.end('user' + id);
     }
-  })
-})
-server.listen(process.argv[2] || 8080)
+  },
+  DELETE: {
+    '/users/:id': (req, res, id) => {
+      res.end('deleted user' + id)
+    }
+  }
+};
+
+connect()
+  .use(router(routes))
+  .listen(3000, _ => {
+    console.log('server run 3000!~~')
+  });
--- router/router.js
+++ router/router.js
@@ -1,10 +1,10 @@
 /*
-* black 2019/1/24
-* router中间件
-* */
-const {URL} = require('url');
+ * black 2019/1/24
+ * router中间件
+ * */
+const URL = require('url');
 
-module.exports = function route(obj) {
+exports.router = function (obj) {
   return function (req, res, next) {
     // 先看下该请求方法是否已定义
     if (!obj[req.method]) {
@@ -12,18 +12,17 @@ module.exports = function route(obj) {
       return
     }
     let routes = obj[req.method];
-    let url = new URL(req.url);
-    console.log(url);
+    let url = URL.parse(req.url);
     // Object.keys() 返回一个所有元素为字符串的数组，其元素来自于从给定的object上面可直接枚举的属性。这些属性的顺序与手动遍历该对象属性时的一致。
     let paths = Object.keys(routes);
-
     for (let i = 0; i < paths.length; i++) {
       let path = paths[i];
       let fn = routes[path];
-      path = path.replace(/\//g,'\\/').replace(/:(\w+)/g,'([^\\/]+)');
-      let re = new RegExp('^'+path+'$');
+      // 处理url，用来和正则匹配
+      path = path.replace(/\//g, '\\/').replace(/:(\w+)/g, '([^\\/]+)');
+      let re = new RegExp('^' + path + '$');
       let captures = url.pathname.match(re);
-      if(captures) {
+      if (captures) {
         let args = [req, res].concat(captures.slice(1));
         fn.apply(null, args);
         return;
