b804c61e2969c5ae335cf22cb2ebe7de4da6f58a
Yinyan <751410306@qq.com>
2019-01-24 18:12:18 +0800

    111

---
+++ router/router.js
@@ -0,0 +1,34 @@
+/*
+* black 2019/1/24
+* router中间件
+* */
+const {URL} = require('url');
+
+module.exports = function route(obj) {
+  return function (req, res, next) {
+    // 先看下该请求方法是否已定义
+    if (!obj[req.method]) {
+      next();
+      return
+    }
+    let routes = obj[req.method];
+    let url = new URL(req.url);
+    console.log(url);
+    // Object.keys() 返回一个所有元素为字符串的数组，其元素来自于从给定的object上面可直接枚举的属性。这些属性的顺序与手动遍历该对象属性时的一致。
+    let paths = Object.keys(routes);
+
+    for (let i = 0; i < paths.length; i++) {
+      let path = paths[i];
+      let fn = routes[path];
+      path = path.replace(/\//g,'\\/').replace(/:(\w+)/g,'([^\\/]+)');
+      let re = new RegExp('^'+path+'$');
+      let captures = url.pathname.match(re);
+      if(captures) {
+        let args = [req, res].concat(captures.slice(1));
+        fn.apply(null, args);
+        return;
+      }
+      next()
+    }
+  }
+};
